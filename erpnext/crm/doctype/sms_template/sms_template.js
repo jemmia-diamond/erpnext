78ade786a9a747c571d66c44177d9993ed40f5a8
JEMMIA <jemmia@MacBook-Air-cua-JEMMIA.local>
2025-05-15 10:31:13 +0700

    feat: init sms doctype

---
+++ erpnext/crm/doctype/sms_template/sms_template.js
@@ -0,0 +1,10 @@
+// Copyright (c) 2025, Frappe Technologies Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SMS Template", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+
