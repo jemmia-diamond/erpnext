1d9ccec37ab0466f4a125c7231249c219671c6ce
TungLam111 <lamtungphan8@gmail.com>
2025-07-08 15:53:43 +0700

    Add try catch

--- erpnext/crm/doctype/lead_product/lead_product_dao.py
+++ erpnext/crm/doctype/lead_product/lead_product_dao.py
@@ -11,15 +11,21 @@ def get_products_in_names(product_names):
 	return products
 
 def get_lead_product(product_type):
-	return frappe.get_doc(
-		"Lead Product", {
-			"product_type": product_type
-		}
-	)
+    try:
+        return frappe.get_doc(
+			"Lead Product", {
+				"product_type": product_type
+			}
+		)
+    except Exception as e:
+        return None
 
 def create_lead_product(product_type):
-	new_lead_product = frappe.new_doc("Lead Product")
-	new_lead_product.update({
-		"product_type": product_type
-	})
-	return new_lead_product.save()
+	try:	
+		new_lead_product = frappe.new_doc("Lead Product")
+		new_lead_product.update({
+			"product_type": product_type
+		})
+		return new_lead_product.save()
+	except Exception as e:
+		return None
