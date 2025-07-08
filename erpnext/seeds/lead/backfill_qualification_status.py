45286720a9c6b24b736d10caaa07328e30a44dd8
NhatNam1607 <nhatnam160701@gmail.com>
2025-07-08 15:52:52 +0700

    set user default is tech

--- erpnext/seeds/lead/backfill_qualification_status.py
+++ erpnext/seeds/lead/backfill_qualification_status.py
@@ -5,7 +5,7 @@ def execute():
     try: 
         frappe.db.sql("""
             UPDATE `tabLead`
-            SET qualification_status = 'Qualified', qualified_by = 'Administrator'
+            SET qualification_status = 'Qualified', qualified_by = 'tech@jemmia.vn'
             WHERE qualified_lead_date IS NOT NULL
             """)
     except Exception:
@@ -13,4 +13,3 @@ def execute():
         print("Failed to update qualified lead date")
         return
     frappe.db.commit()
-    
