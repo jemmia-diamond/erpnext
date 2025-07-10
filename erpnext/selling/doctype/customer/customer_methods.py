b20eff3992c7085e57d51618dfb3b2262e59e3cb
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-10 14:19:08 +0700

    handle exception

--- erpnext/selling/doctype/customer/customer_methods.py
+++ erpnext/selling/doctype/customer/customer_methods.py
@@ -1,12 +1,16 @@
 import frappe
 
 def map_customer_lead():
-    frappe.db.sql("""
-        UPDATE tabCustomer c
-	        JOIN `tabDynamic Link` dl ON dl.link_doctype = 'Customer' AND dl.link_name = c.name
-	        JOIN tabContact co ON dl.parent = co.name
-	        JOIN tabLead l ON co.phone = l.phone AND LENGTH(l.phone) >= 10 AND LENGTH(co.phone) >= 10
-        SET c.lead_name = l.name
-        WHERE c.lead_name IS NULL;
-    """)
+    try:
+        frappe.db.sql("""
+            UPDATE tabCustomer c
+	            JOIN `tabDynamic Link` dl ON dl.link_doctype = 'Customer' AND dl.link_name = c.name
+	            JOIN tabContact co ON dl.parent = co.name
+	            JOIN tabLead l ON co.phone = l.phone AND LENGTH(l.phone) >= 10 AND LENGTH(co.phone) >= 10
+            SET c.lead_name = l.name
+            WHERE c.lead_name IS NULL;
+        """)
+    except Exception:
+        frappe.db.rollback()
+        return
     frappe.db.commit()
