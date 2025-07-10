0ab31fb22de6978e278d9a12a9e8ed5e38396bf7
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-10 14:15:15 +0700

    handle phone length

--- erpnext/selling/doctype/customer/customer_methods.py
+++ erpnext/selling/doctype/customer/customer_methods.py
@@ -3,9 +3,9 @@ import frappe
 def map_customer_lead():
     frappe.db.sql("""
         UPDATE tabCustomer c
-        	JOIN `tabDynamic Link` dl ON dl.link_doctype = 'Customer' AND dl.link_name = c.name
-        	JOIN tabContact co ON dl.parent = co.name
-        	JOIN tabLead l ON co.phone = l.phone
+	        JOIN `tabDynamic Link` dl ON dl.link_doctype = 'Customer' AND dl.link_name = c.name
+	        JOIN tabContact co ON dl.parent = co.name
+	        JOIN tabLead l ON co.phone = l.phone AND LENGTH(l.phone) >= 10 AND LENGTH(co.phone) >= 10
         SET c.lead_name = l.name
         WHERE c.lead_name IS NULL;
     """)
