da56be2c31ec6a60a585b6b7b389f4e89f915293
Hung Tran <hungtranthanh0302@gmail.com>
2025-07-22 10:54:13 +0700

    feature: create sales order notification

--- erpnext/config/config.py
+++ erpnext/config/config.py
@@ -7,4 +7,7 @@ class BaseConfig:
     DATE_ASSIGN_LEAD_OWNER : str = "2025-06-15T14:00:00+00:00"
     STRINGEE_API_KEY_SID: str = frappe.conf.get("stringee_api_key_sid")
     STRINGEE_API_KEY_SECRET: str = frappe.conf.get("stringee_api_key_secret")
+    FN_BEARER_TOKEN: str = frappe.conf.get("fn_bearer_token")
+    FN_BASE_URL: str = frappe.conf.get("fn_base_url")
+
 config = BaseConfig()
