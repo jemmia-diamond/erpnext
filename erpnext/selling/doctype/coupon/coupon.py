b2b3f10d3fe0abc17f9f6629168ffbb3ced9c003
Hoàng Hưng <hoanghung@MacBook-Pro-cua-Hoang.local>
2025-07-28 11:59:32 +0700

    add coupon type

--- erpnext/selling/doctype/coupon/coupon.py
+++ erpnext/selling/doctype/coupon/coupon.py
@@ -24,7 +24,7 @@ class Coupon(Document):
 		parenttype: DF.Data
 		payment_status: DF.Literal["Paid", "Pending"]
 		total_price: DF.Currency
-		user: DF.Data | None
+		user: DF.Link | None
 	# end: auto-generated types
 	from typing import TYPE_CHECKING
 	if TYPE_CHECKING:
@@ -43,7 +43,12 @@ def update_all_customers_coupon_code():
 	try:
 		priority_bearer_token: str = config.PRIORITY_BEARER_TOKEN
 		priority_base_url: str = config.PRIORITY_BASE_URL
-		response = requests.get(f"{priority_base_url}/sync-crm/coupon-ref?updatedInCrm=false", json={}, headers={"Authorization": f"Bearer {priority_bearer_token}"})
+
+		response = requests.get(
+			f"{priority_base_url}/sync-crm/coupon-ref?updatedInCrm=false",
+			json={},
+			headers={"Authorization": f"Bearer {priority_bearer_token}"}
+		)
 
 		if response.status_code != 200:
 			frappe.throw(_("Failed to fetch data from priority API"))
@@ -51,8 +56,8 @@ def update_all_customers_coupon_code():
 		results = response.json()
 
 		for result in results:
-			haravan_id = result.get("haravanId")
-			owner_name = result.get("ownerName")
+			owner_haravan_id = result.get("haravanId")
+			user_haravan_id = result.get("userHaravanId")
 			haravan_coupon_code = result.get("couponHaravanCode")
 			haravan_coupon_id = result.get("couponHaravanId")
 			total_price = flt(result.get("totalPrice", 0))
@@ -62,19 +67,17 @@ def update_all_customers_coupon_code():
 			coupon_status = result.get("couponStatus", "Used")
 			coupon_type = result.get("couponType", "Invite")
 
-			if not haravan_id or not owner_name or not haravan_coupon_code:
+			if not owner_haravan_id or not haravan_coupon_code:
 				continue
 
-			customer = frappe.get_value("Customer", {"haravan_id": haravan_id}, "name")
+			customer = frappe.get_value("Customer", {"haravan_id": owner_haravan_id}, "name")
 			if not customer:
-				frappe.logger().info(f"Skipped: Customer with haravan_id {haravan_id} not found.")
+				frappe.logger().info(f"Skipped: Customer with haravan_id {owner_haravan_id} not found.")
 				continue
 
-			existing_coupon_by_name = frappe.get_value("Coupon", {"haravan_coupon_id": haravan_coupon_id}, "name")
-			if existing_coupon_by_name:
-				continue
+			user = frappe.get_value("Customer", {"haravan_id": user_haravan_id}, "name") if user_haravan_id else None
 
-			existing_coupon = frappe.get_value("Coupon", {"coupon_name": haravan_coupon_code}, "name")
+			existing_coupon = frappe.get_value("Coupon", {"haravan_coupon_id": haravan_coupon_id}, "name")
 
 			if existing_coupon:
 				coupon_doc = frappe.get_doc("Coupon", existing_coupon)
@@ -84,22 +87,22 @@ def update_all_customers_coupon_code():
 			coupon_doc.haravan_coupon_id = haravan_coupon_id
 			coupon_doc.coupon_name = haravan_coupon_code
 			coupon_doc.customer = customer
-			coupon_doc.user = used_by_name  
+			coupon_doc.user = user
 			coupon_doc.coupon_type = coupon_type
 			coupon_doc.total_price = total_price
 			coupon_doc.cashback_ref = cashback_ref
 			coupon_doc.payment_status = payment_status
 			coupon_doc.parent = customer
 			coupon_doc.parentfield = "coupon_table"
-			coupon_doc.parenttype = "Customer"  
-			
+			coupon_doc.parenttype = "Customer"
+
 			if not existing_coupon:
 				coupon_doc.insert(ignore_permissions=True)
 			else:
 				coupon_doc.save(ignore_permissions=True)
 
 		frappe.db.commit()
-		
+
 	except Exception as e:
 		frappe.db.rollback()
 		frappe.log_error(frappe.get_traceback(), "Error updating coupon codes")
