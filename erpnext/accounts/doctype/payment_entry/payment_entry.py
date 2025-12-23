af9f83b074bc2cd24914fb19f085186fcea31ca5
BinDK <sarakenny123@yahoo.com>
2025-12-23 17:54:48 +0700

    enhance(sales-order): tolerate 1k vnd

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -2256,7 +2256,8 @@ def validate_and_misa_field(payment_entry_name):
 			return
 
 	total_allocated = sum(flt(ref.allocated_amount) for ref in doc.references)
-	if flt(doc.paid_amount) != flt(total_allocated):
+	difference = abs(flt(total_allocated) - flt(doc.paid_amount))
+	if difference > 1000:
 		frappe.throw(
 			_("Số tiền thanh toán ({0}) phải bằng tổng số tiền phân bổ ({1}).").format(
 				fmt_money(doc.paid_amount, currency=doc.paid_from_account_currency),
