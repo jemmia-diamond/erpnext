ec054379faba32ee975e5d3319b08f99023bcd47
BinDK <sarakenny123@yahoo.com>
2025-12-23 17:43:30 +0700

    enhance(payment-entry): not auto subbmit if allocated amount mismatch

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -2255,6 +2255,15 @@ def validate_and_misa_field(payment_entry_name):
 		if not doc.verified_by:
 			return
 
+	total_allocated = sum(flt(ref.allocated_amount) for ref in doc.references)
+	if flt(doc.paid_amount) != flt(total_allocated):
+		frappe.throw(
+			_("Số tiền thanh toán ({0}) phải bằng tổng số tiền phân bổ ({1}).").format(
+				fmt_money(doc.paid_amount, currency=doc.paid_from_account_currency),
+				fmt_money(total_allocated, currency=doc.paid_from_account_currency)
+			)
+		)
+
 	frappe.db.sql("""
 		UPDATE `tabPayment Entry`
 		SET docstatus = 1,
