361e8584a5c09d8d072b2157463dfa29589434e2
BinDK <sarakenny123@yahoo.com>
2026-01-11 19:50:38 +0700

    fix(payment-entry): introduce phone number validation

--- erpnext/public/js/utils/contact_address_quick_entry.js
+++ erpnext/public/js/utils/contact_address_quick_entry.js
@@ -11,6 +11,57 @@ frappe.ui.form.ContactAddressQuickEntryForm = class ContactAddressQuickEntryForm
 	render_dialog() {
 		this.mandatory = this.mandatory.concat(this.get_variant_fields());
 		super.render_dialog();
+		this.setup_mobile_validation();
+	}
+
+	setup_mobile_validation() {
+		const me = this;
+		this.dialog.get_primary_btn().prop('disabled', true);		
+		if (!this.dialog.fields_dict.mobile_number) {
+			return;
+		}
+
+		const check_btn = $(`
+			<button class="btn btn-sm btn-secondary check-mobile-btn" style="margin-top: 5px;">
+				${__("Check")}
+			</button>
+		`);
+		
+		this.dialog.fields_dict.mobile_number.$wrapper.append(check_btn);
+		this.dialog.fields_dict.mobile_number.$input.on('input', function() {
+			const mobile_value = $(this).val().trim();
+			const check_btn = me.dialog.fields_dict.mobile_number.$wrapper.find('.check-mobile-btn');
+			
+			if (!mobile_value) {
+				me.dialog.get_primary_btn().prop('disabled', true);
+				check_btn.hide();
+			} else {
+				me.dialog.get_primary_btn().prop('disabled', true);
+				check_btn.show().text(__("Check")).prop('disabled', false);
+			}
+		});
+
+		check_btn.on('click', function() {
+			const mobile_no = me.dialog.get_value('mobile_number');
+			if (!mobile_no || !mobile_no.trim()) {
+				frappe.msgprint(__("Please enter a mobile number first"));
+				return;
+			}
+
+			const trimmed_mobile = mobile_no.trim();			
+			$(this).text(__("Checking")).prop('disabled', true);
+
+			me.validate_mobile_number(trimmed_mobile).then(() => {
+				me.dialog.get_primary_btn().prop('disabled', false);
+				$(this).hide();
+				frappe.show_alert({
+					message: __("Phone number available"),
+					indicator: "green"
+				});
+			}).catch(() => {
+				$(this).text(__("Check")).prop('disabled', false);
+			});
+		});
 	}
 
 	insert() {
@@ -30,6 +81,39 @@ frappe.ui.form.ContactAddressQuickEntryForm = class ContactAddressQuickEntryForm
 		});
 
 		return super.insert();
+	}
+
+	validate_mobile_number(mobile_no) {
+		return new Promise((resolve, reject) => {
+			frappe.call({
+				method: "frappe.client.get_list",
+				args: {
+					doctype: "Customer",
+					or_filters: [
+						["mobile_no", "=", mobile_no],
+						["phone", "=", mobile_no]
+					],
+					fields: ["name", "customer_name"]
+				},
+				callback: function(r) {
+					if (r.message && r.message.length > 0) {
+						const existing_customer = r.message[0];
+						frappe.msgprint({
+							title: __("Duplicate Mobile Number"),
+							message: __("Mobile number {0} already exists for customer: {1}", 
+								[mobile_no, existing_customer.customer_name || existing_customer.name]),
+							indicator: "orange"
+						});
+						reject();
+					} else {
+						resolve();
+					}
+				},
+				error: function() {
+					reject();
+				}
+			});
+		});
 	}
 
 	get_variant_fields() {
