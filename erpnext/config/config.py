1423bfe26d84d353fb592dc230bd27ab0e058784
BinDK <sarakenny123@yahoo.com>
2026-03-31 13:18:29 +0700

    Merge pull request #58 from jemmia-diamond/feature/stringee_call_log
    feature: play Stringee call record

--- erpnext/config/config.py
+++ erpnext/config/config.py
@@ -8,5 +8,7 @@ class BaseConfig:
     AI_HUB_WEBHOOK : str = frappe.conf.get("ai_hub_webhook")
     DEFAULT_MAIL_OWNER : str = frappe.conf.get("default_mail_lead_owner")
     DATE_ASSIGN_LEAD_OWNER : str = "2025-06-15T14:00:00+00:00"
+    STRINGEE_API_KEY_SID: str = frappe.conf.get("stringee_api_key_sid")
+    STRINGEE_API_KEY_SECRET: str = frappe.conf.get("stringee_api_key_secret")
 
 config = BaseConfig()
--- erpnext/telephony/doctype/call_log/call_log.js
+++ erpnext/telephony/doctype/call_log/call_log.js
@@ -3,14 +3,23 @@
 
 frappe.ui.form.on("Call Log", {
 	refresh: function (frm) {
-		frm.events.setup_recording_audio_control(frm);
+
 		const incoming_call = frm.doc.type == "Incoming";
 		frm.add_custom_button(incoming_call ? __("Callback") : __("Call Again"), () => {
 			const number = incoming_call ? frm.doc.from : frm.doc.to;
 			frappe.phone_call.handler(number, frm);
 		});
+
+		try {
+			frappe.call({
+				method: "erpnext.telephony.doctype.call_log.call_log.get_access_token",
+				callback: function (r) {
+					frm.events.setup_recording_audio_control(frm, r.message);
+				},
+			});
+		} catch {}
 	},
-	setup_recording_audio_control(frm) {
+	setup_recording_audio_control(frm, accessToken) {
 		const recording_wrapper = frm.get_field("recording_html").$wrapper;
 		if (!frm.doc.recording_url || frm.doc.recording_url == "null") {
 			recording_wrapper.empty();
@@ -19,7 +28,7 @@ frappe.ui.form.on("Call Log", {
 			recording_wrapper.html(`
 				<audio
 					controls
-					src="${frm.doc.recording_url}">
+					src="${frm.doc.recording_url}?access_token=${accessToken}">
 				</audio>
 			`);
 		}
