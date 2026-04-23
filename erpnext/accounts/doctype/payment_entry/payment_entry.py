d6e06ab0528d526717fca17a40920ffe2fb5affb
BinDK <sarakenny123@yahoo.com>
2026-04-23 16:59:34 +0700

    chore: temporary not using frappe validate when allocate

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -233,7 +233,7 @@ class PaymentEntry(AccountsController):
 		self.set_remarks()
 		self.validate_duplicate_entry()
 		self.validate_payment_type_with_outstanding()
-		self.validate_allocated_amount()
+		# self.validate_allocated_amount()
 		self.validate_paid_invoices()
 		self.ensure_supplier_is_not_blocked()
 		self.set_tax_withholding()
