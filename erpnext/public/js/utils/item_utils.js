354ebe6b3314a2b8ad99db7e4df7a9fc9fe72624
tunglamphandam <lamtungphan8@gmail.com>
2026-05-29 17:24:17 +0700

    feat: implement jewelry item validation logic for serial numbers in Sales Order

---
+++ erpnext/public/js/utils/item_utils.js
@@ -0,0 +1,32 @@
+frappe.provide("erpnext.utils.item");
+
+$.extend(erpnext.utils.item, {
+	SKU_LENGTH: {
+		JEWELRY: 21
+	},
+
+	SKU_PREFIX: {
+		TEMPORARY_JEWELRY: "SPT",
+		DIAMOND: "AJ",
+		DIAMOND_TEMPORARY: "GIA",
+		GIFT: "QT"
+	},
+
+	HRV_PRODUCT_TYPE: {
+		PLAIN_CHAIN: "Dây Chuyền Trơn"
+	},
+
+	isGiftItem: function(item) {
+		return item.sku?.startsWith(erpnext.utils.item.SKU_PREFIX.GIFT);
+	},
+
+	isJewelryItem: function(item) {
+		return item.sku?.startsWith(erpnext.utils.item.SKU_PREFIX.TEMPORARY_JEWELRY) || 
+			   item.sku?.length === erpnext.utils.item.SKU_LENGTH.JEWELRY;
+	},
+
+	isDiamondItem: function(item) {
+		return item.sku?.startsWith(erpnext.utils.item.SKU_PREFIX.DIAMOND) || 
+			   item.sku?.startsWith(erpnext.utils.item.SKU_PREFIX.DIAMOND_TEMPORARY);
+	}
+});
