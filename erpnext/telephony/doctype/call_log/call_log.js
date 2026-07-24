311993c2c6b5a69b299e0d7749a49472a31ebb03
BinDK <sarakenny123@yahoo.com>
2026-07-24 11:29:57 +0700

    fix(call-log): attachment download looping

--- erpnext/telephony/doctype/call_log/call_log.js
+++ erpnext/telephony/doctype/call_log/call_log.js
@@ -37,7 +37,8 @@ frappe.ui.form.on("Call Log", {
 				const attached_mp3 = attachments.find(f => f.file_url && f.file_name && f.file_name.startsWith("recording_"));
 				if (attached_mp3) {
 					audio_src = attached_mp3.file_url;
-				} else {
+				} else if (!frm.__download_attempted) {
+					frm.__download_attempted = true;
 					frappe.call({
 						method: "erpnext.telephony.doctype.call_log.call_log.download_and_attach_recording",
 						args: { call_log_name: frm.doc.name },
