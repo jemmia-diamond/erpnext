3b60760e93277a978376e4272e67639a51c9f975
BinDK <sarakenny123@yahoo.com>
2026-07-29 13:33:28 +0700

    chore(appointment): added performed_by field

--- erpnext/crm/doctype/appointment/appointment.py
+++ erpnext/crm/doctype/appointment/appointment.py
@@ -47,6 +47,7 @@ class Appointment(Document):
 		offline_sales_name: DF.Data | None
 		order_status: DF.Literal["Kh\u00e1ch \u0111\u00e3 mua h\u00e0ng", "Kh\u00e1ch h\u1eb9n \u0111\u1ebfn c\u1eeda h\u00e0ng", "Kh\u00e1ch ch\u01b0a mua h\u00e0ng", "Kh\u00e1ch kh\u00f4ng \u0111\u1ebfn c\u1eeda h\u00e0ng", "Kh\u00e1ch ho\u00e3n l\u1ea1i ng\u00e0y \u0111\u1ebfn c\u1eeda h\u00e0ng", "Kh\u00e1ch \u0111\u00e3 \u0111\u1ebfn c\u1eeda h\u00e0ng"]
 		party: DF.DynamicLink | None
+		performed_by: DF.Data | None
 		policies: DF.TableMultiSelect[AppointmentPolicy]
 		policy: DF.LongText | None
 		preferred_products: DF.TableMultiSelect[LeadProductItem]
