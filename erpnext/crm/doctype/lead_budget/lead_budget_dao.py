10215a1d7d562a1d509a7f2cdcd221a5bb7a9f95
BinDK <sarakenny123@yahoo.com>
2026-08-13 15:18:53 +0700

    chore(lead): refactor lead_method on new budget_field

--- erpnext/crm/doctype/lead_budget/lead_budget_dao.py
+++ erpnext/crm/doctype/lead_budget/lead_budget_dao.py
@@ -1,7 +1,9 @@
 import frappe
 
 
-def find_range_budget(budget_from: int | None, budget_to:int | None):
+def find_range_budget(budget_name: str | None, budget_from: int | None, budget_to: int | None):
+	if budget_name and frappe.db.exists("Lead Budget", budget_name):
+		return frappe._dict({"name": budget_name})
 
 	if budget_from is None and budget_to is None:
 		return None
